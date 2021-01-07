981923eb7224838dc45f6b2988a664b1c3f13a68
Putra Langkat <pucukpisang97@gmail.com>
2021-01-07 13:20:50 +0700

    Datatables yarja added

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,11 +5,9 @@
 <div class="row">
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
         <div class="card shadow">
-            <div class="card-header">
-                <h4>Testing aja</h4>
-            </div>
             <div class="card-body">
-                <table class="table table-bordered table-sm">
+                <button id="add" class="btn btn-sm btn-primary mb-3"><i class="fa fa-plus"></i> Add</button>
+                <table class="table table-bordered table-sm" id="table">
                     <thead class="text-center">
                         <tr>
                             <th>#</th>
@@ -62,3 +60,19 @@
     </div>
 </div>
 @endsection
+
+@push('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/datatables/css/dataTables.bootstrap4.min.css') }}">
+@endpush
+
+@push('js')
+<script src="{{ asset('assets/vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/js/DataTables.bootstrap4.min.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('#table').DataTable({
+            'ordering': false,
+        });
+    });
+</script>
+@endpush
